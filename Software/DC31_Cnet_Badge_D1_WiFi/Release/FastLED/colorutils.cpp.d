25a56858d055571c5d17c3aa750f38bebf69daca
slash128 <jg@granatlabs.com>
2023-03-07 18:50:50 -0800

    Add files via upload

---
+++ Software/DC31_Cnet_Badge_D1_WiFi/Release/FastLED/colorutils.cpp.d
@@ -0,0 +1,95 @@
+C:\\Users\\jgranat\\AppData\\Local\\Temp\\VMBuilds\\DC31_Cnet_Badge_D1_WiFi\\esp8266_d1_mini\\Release\\FastLED\\colorutils.cpp.o: \
+ c:\\Users\\jgranat\\My\ Drive\\Dev\\Arduino\\libraries\\FastLED\\colorutils.cpp \
+ c:\\Users\\jgranat\\AppData\\Local\\Arduino15\\packages\\esp8266\\hardware\\esp8266\\2.4.1/tools/sdk/libc/xtensa-lx106-elf/include/stdint.h \
+ c:\\Users\\jgranat\\AppData\\Local\\Arduino15\\packages\\esp8266\\hardware\\esp8266\\2.4.1/tools/sdk/libc/xtensa-lx106-elf/include/machine/_default_types.h \
+ c:\\Users\\jgranat\\AppData\\Local\\Arduino15\\packages\\esp8266\\hardware\\esp8266\\2.4.1/tools/sdk/libc/xtensa-lx106-elf/include/sys/features.h \
+ c:\\Users\\jgranat\\AppData\\Local\\Arduino15\\packages\\esp8266\\hardware\\esp8266\\2.4.1/tools/sdk/libc/xtensa-lx106-elf/include/sys/_intsup.h \
+ c:\\Users\\jgranat\\AppData\\Local\\Arduino15\\packages\\esp8266\\hardware\\esp8266\\2.4.1/tools/sdk/libc/xtensa-lx106-elf/include/math.h \
+ c:\\Users\\jgranat\\AppData\\Local\\Arduino15\\packages\\esp8266\\hardware\\esp8266\\2.4.1/tools/sdk/libc/xtensa-lx106-elf/include/sys/reent.h \
+ c:\\Users\\jgranat\\AppData\\Local\\Arduino15\\packages\\esp8266\\hardware\\esp8266\\2.4.1/tools/sdk/libc/xtensa-lx106-elf/include/_ansi.h \
+ c:\\Users\\jgranat\\AppData\\Local\\Arduino15\\packages\\esp8266\\hardware\\esp8266\\2.4.1/tools/sdk/libc/xtensa-lx106-elf/include/newlib.h \
+ c:\\Users\\jgranat\\AppData\\Local\\Arduino15\\packages\\esp8266\\hardware\\esp8266\\2.4.1/tools/sdk/libc/xtensa-lx106-elf/include/sys/config.h \
+ c:\\Users\\jgranat\\AppData\\Local\\Arduino15\\packages\\esp8266\\hardware\\esp8266\\2.4.1/tools/sdk/libc/xtensa-lx106-elf/include/machine/ieeefp.h \
+ c:\\Users\\jgranat\\AppData\\Local\\Arduino15\\packages\\esp8266\\hardware\\esp8266\\2.4.1/tools/sdk/libc/xtensa-lx106-elf/include/xtensa/config/core-isa.h \
+ c:\\Users\\jgranat\\AppData\\Local\\Arduino15\\packages\\esp8266\\hardware\\esp8266\\2.4.1/tools/sdk/libc/xtensa-lx106-elf/include/sys/_types.h \
+ c:\\Users\\jgranat\\AppData\\Local\\Arduino15\\packages\\esp8266\\hardware\\esp8266\\2.4.1/tools/sdk/libc/xtensa-lx106-elf/include/machine/_types.h \
+ c:\\Users\\jgranat\\AppData\\Local\\Arduino15\\packages\\esp8266\\hardware\\esp8266\\2.4.1/tools/sdk/libc/xtensa-lx106-elf/include/sys/lock.h \
+ c:\\Users\\jgranat\\AppData\\Local\\Arduino15\\packages\\esp8266\\hardware\\esp8266\\2.4.1/tools/sdk/libc/xtensa-lx106-elf/include/_ansi.h \
+ c:\\Users\\jgranat\\My\ Drive\\Dev\\Arduino\\libraries\\FastLED\\FastLED.h \
+ c:\\Users\\jgranat\\My\ Drive\\Dev\\Arduino\\libraries\\FastLED\\cpp_compat.h \
+ c:\\Users\\jgranat\\My\ Drive\\Dev\\Arduino\\libraries\\FastLED\\fastled_config.h \
+ c:\\Users\\jgranat\\My\ Drive\\Dev\\Arduino\\libraries\\FastLED\\led_sysdefs.h \
+ c:\\Users\\jgranat\\My\ Drive\\Dev\\Arduino\\libraries\\FastLED\\platforms/esp/8266/led_sysdefs_esp8266.h \
+ c:\\Users\\jgranat\\AppData\\Local\\Arduino15\\packages\\esp8266\\hardware\\esp8266\\2.4.1\\cores\\esp8266/Arduino.h \
+ c:\\Users\\jgranat\\AppData\\Local\\Arduino15\\packages\\esp8266\\hardware\\esp8266\\2.4.1/tools/sdk/libc/xtensa-lx106-elf/include/stdlib.h \
+ c:\\Users\\jgranat\\AppData\\Local\\Arduino15\\packages\\esp8266\\hardware\\esp8266\\2.4.1/tools/sdk/libc/xtensa-lx106-elf/include/sys/cdefs.h \
+ c:\\Users\\jgranat\\AppData\\Local\\Arduino15\\packages\\esp8266\\hardware\\esp8266\\2.4.1/tools/sdk/libc/xtensa-lx106-elf/include/machine/stdlib.h \
+ c:\\Users\\jgranat\\AppData\\Local\\Arduino15\\packages\\esp8266\\hardware\\esp8266\\2.4.1/tools/sdk/libc/xtensa-lx106-elf/include/alloca.h \
+ c:\\Users\\jgranat\\AppData\\Local\\Arduino15\\packages\\esp8266\\hardware\\esp8266\\2.4.1/tools/sdk/libc/xtensa-lx106-elf/include/stdio.h \
+ c:\\Users\\jgranat\\AppData\\Local\\Arduino15\\packages\\esp8266\\hardware\\esp8266\\2.4.1/tools/sdk/libc/xtensa-lx106-elf/include/sys/types.h \
+ c:\\Users\\jgranat\\AppData\\Local\\Arduino15\\packages\\esp8266\\hardware\\esp8266\\2.4.1/tools/sdk/libc/xtensa-lx106-elf/include/machine/types.h \
+ c:\\Users\\jgranat\\AppData\\Local\\Arduino15\\packages\\esp8266\\hardware\\esp8266\\2.4.1/tools/sdk/libc/xtensa-lx106-elf/include/sys/stdio.h \
+ c:\\Users\\jgranat\\AppData\\Local\\Arduino15\\packages\\esp8266\\hardware\\esp8266\\2.4.1/tools/sdk/libc/xtensa-lx106-elf/include/string.h \
+ c:\\Users\\jgranat\\AppData\\Local\\Arduino15\\packages\\esp8266\\hardware\\esp8266\\2.4.1/tools/sdk/libc/xtensa-lx106-elf/include/sys/string.h \
+ c:\\Users\\jgranat\\AppData\\Local\\Arduino15\\packages\\esp8266\\hardware\\esp8266\\2.4.1\\cores\\esp8266/stdlib_noniso.h \
+ c:\\Users\\jgranat\\AppData\\Local\\Arduino15\\packages\\esp8266\\hardware\\esp8266\\2.4.1\\cores\\esp8266/binary.h \
+ c:\\Users\\jgranat\\AppData\\Local\\Arduino15\\packages\\esp8266\\hardware\\esp8266\\2.4.1\\cores\\esp8266/esp8266_peri.h \
+ c:\\Users\\jgranat\\AppData\\Local\\Arduino15\\packages\\esp8266\\hardware\\esp8266\\2.4.1/tools/sdk/include/c_types.h \
+ c:\\Users\\jgranat\\AppData\\Local\\Arduino15\\packages\\esp8266\\hardware\\esp8266\\2.4.1\\cores\\esp8266/twi.h \
+ c:\\Users\\jgranat\\AppData\\Local\\Arduino15\\packages\\esp8266\\hardware\\esp8266\\2.4.1\\cores\\esp8266/Arduino.h \
+ c:\\Users\\jgranat\\AppData\\Local\\Arduino15\\packages\\esp8266\\hardware\\esp8266\\2.4.1\\cores\\esp8266/core_esp8266_features.h \
+ c:\\Users\\jgranat\\AppData\\Local\\Arduino15\\packages\\esp8266\\hardware\\esp8266\\2.4.1\\cores\\esp8266/pgmspace.h \
+ c:\\Users\\jgranat\\AppData\\Local\\Arduino15\\packages\\esp8266\\hardware\\esp8266\\2.4.1/tools/sdk/include/ets_sys.h \
+ c:\\Users\\jgranat\\AppData\\Local\\Arduino15\\packages\\esp8266\\hardware\\esp8266\\2.4.1/tools/sdk/include/c_types.h \
+ c:\\Users\\jgranat\\AppData\\Local\\Arduino15\\packages\\esp8266\\hardware\\esp8266\\2.4.1/tools/sdk/include/eagle_soc.h \
+ c:\\Users\\jgranat\\AppData\\Local\\Arduino15\\packages\\esp8266\\hardware\\esp8266\\2.4.1/tools/sdk/include/osapi.h \
+ c:\\Users\\jgranat\\AppData\\Local\\Arduino15\\packages\\esp8266\\hardware\\esp8266\\2.4.1\\cores\\esp8266/user_config.h \
+ c:\\Users\\jgranat\\AppData\\Local\\Arduino15\\packages\\esp8266\\hardware\\esp8266\\2.4.1\\cores\\esp8266/WCharacter.h \
+ c:\\Users\\jgranat\\AppData\\Local\\Arduino15\\packages\\esp8266\\hardware\\esp8266\\2.4.1\\cores\\esp8266/WString.h \
+ c:\\Users\\jgranat\\AppData\\Local\\Arduino15\\packages\\esp8266\\hardware\\esp8266\\2.4.1\\cores\\esp8266/pgmspace.h \
+ c:\\Users\\jgranat\\AppData\\Local\\Arduino15\\packages\\esp8266\\hardware\\esp8266\\2.4.1\\cores\\esp8266/HardwareSerial.h \
+ c:\\Users\\jgranat\\AppData\\Local\\Arduino15\\packages\\esp8266\\hardware\\esp8266\\2.4.1/tools/sdk/libc/xtensa-lx106-elf/include/inttypes.h \
+ c:\\Users\\jgranat\\AppData\\Local\\Arduino15\\packages\\esp8266\\hardware\\esp8266\\2.4.1\\cores\\esp8266/Stream.h \
+ c:\\Users\\jgranat\\AppData\\Local\\Arduino15\\packages\\esp8266\\hardware\\esp8266\\2.4.1\\cores\\esp8266/Print.h \
+ c:\\Users\\jgranat\\AppData\\Local\\Arduino15\\packages\\esp8266\\hardware\\esp8266\\2.4.1\\cores\\esp8266/Printable.h \
+ c:\\Users\\jgranat\\AppData\\Local\\Arduino15\\packages\\esp8266\\hardware\\esp8266\\2.4.1\\cores\\esp8266/uart.h \
+ c:\\Users\\jgranat\\AppData\\Local\\Arduino15\\packages\\esp8266\\hardware\\esp8266\\2.4.1\\cores\\esp8266/Esp.h \
+ c:\\Users\\jgranat\\AppData\\Local\\Arduino15\\packages\\esp8266\\hardware\\esp8266\\2.4.1\\cores\\esp8266/Updater.h \
+ c:\\Users\\jgranat\\AppData\\Local\\Arduino15\\packages\\esp8266\\hardware\\esp8266\\2.4.1\\cores\\esp8266/flash_utils.h \
+ c:\\Users\\jgranat\\AppData\\Local\\Arduino15\\packages\\esp8266\\hardware\\esp8266\\2.4.1\\cores\\esp8266/MD5Builder.h \
+ c:\\Users\\jgranat\\AppData\\Local\\Arduino15\\packages\\esp8266\\hardware\\esp8266\\2.4.1\\cores\\esp8266/WString.h \
+ c:\\Users\\jgranat\\AppData\\Local\\Arduino15\\packages\\esp8266\\hardware\\esp8266\\2.4.1\\cores\\esp8266/Stream.h \
+ c:\\Users\\jgranat\\AppData\\Local\\Arduino15\\packages\\esp8266\\hardware\\esp8266\\2.4.1\\cores\\esp8266/md5.h \
+ c:\\Users\\jgranat\\AppData\\Local\\Arduino15\\packages\\esp8266\\hardware\\esp8266\\2.4.1\\cores\\esp8266/debug.h \
+ c:\\Users\\jgranat\\AppData\\Local\\Arduino15\\packages\\esp8266\\hardware\\esp8266\\2.4.1\\variants\\d1_mini/pins_arduino.h \
+ c:\\Users\\jgranat\\AppData\\Local\\Arduino15\\packages\\esp8266\\hardware\\esp8266\\2.4.1\\variants\\d1_mini/../generic/common.h \
+ c:\\Users\\jgranat\\My\ Drive\\Dev\\Arduino\\libraries\\FastLED\\fastled_delay.h \
+ c:\\Users\\jgranat\\My\ Drive\\Dev\\Arduino\\libraries\\FastLED\\bitswap.h \
+ c:\\Users\\jgranat\\My\ Drive\\Dev\\Arduino\\libraries\\FastLED\\controller.h \
+ c:\\Users\\jgranat\\My\ Drive\\Dev\\Arduino\\libraries\\FastLED\\pixeltypes.h \
+ c:\\Users\\jgranat\\My\ Drive\\Dev\\Arduino\\libraries\\FastLED\\lib8tion.h \
+ c:\\Users\\jgranat\\My\ Drive\\Dev\\Arduino\\libraries\\FastLED\\lib8tion/math8.h \
+ c:\\Users\\jgranat\\My\ Drive\\Dev\\Arduino\\libraries\\FastLED\\lib8tion/scale8.h \
+ c:\\Users\\jgranat\\My\ Drive\\Dev\\Arduino\\libraries\\FastLED\\lib8tion/scale8.h \
+ c:\\Users\\jgranat\\My\ Drive\\Dev\\Arduino\\libraries\\FastLED\\lib8tion/random8.h \
+ c:\\Users\\jgranat\\My\ Drive\\Dev\\Arduino\\libraries\\FastLED\\lib8tion/trig8.h \
+ c:\\Users\\jgranat\\My\ Drive\\Dev\\Arduino\\libraries\\FastLED\\color.h \
+ c:\\Users\\jgranat\\My\ Drive\\Dev\\Arduino\\libraries\\FastLED\\fastpin.h \
+ c:\\Users\\jgranat\\My\ Drive\\Dev\\Arduino\\libraries\\FastLED\\fastspi_types.h \
+ c:\\Users\\jgranat\\My\ Drive\\Dev\\Arduino\\libraries\\FastLED\\./dmx.h \
+ c:\\Users\\jgranat\\My\ Drive\\Dev\\Arduino\\libraries\\FastLED\\./FastLED.h \
+ c:\\Users\\jgranat\\My\ Drive\\Dev\\Arduino\\libraries\\FastLED\\platforms.h \
+ c:\\Users\\jgranat\\My\ Drive\\Dev\\Arduino\\libraries\\FastLED\\platforms/esp/8266/fastled_esp8266.h \
+ c:\\Users\\jgranat\\My\ Drive\\Dev\\Arduino\\libraries\\FastLED\\platforms/esp/8266/fastpin_esp8266.h \
+ c:\\Users\\jgranat\\My\ Drive\\Dev\\Arduino\\libraries\\FastLED\\platforms/esp/8266/clockless_esp8266.h \
+ c:\\Users\\jgranat\\My\ Drive\\Dev\\Arduino\\libraries\\FastLED\\platforms/esp/8266/clockless_block_esp8266.h \
+ c:\\Users\\jgranat\\My\ Drive\\Dev\\Arduino\\libraries\\FastLED\\fastled_progmem.h \
+ c:\\Users\\jgranat\\My\ Drive\\Dev\\Arduino\\libraries\\FastLED\\hsv2rgb.h \
+ c:\\Users\\jgranat\\My\ Drive\\Dev\\Arduino\\libraries\\FastLED\\colorutils.h \
+ c:\\Users\\jgranat\\My\ Drive\\Dev\\Arduino\\libraries\\FastLED\\pixelset.h \
+ c:\\Users\\jgranat\\My\ Drive\\Dev\\Arduino\\libraries\\FastLED\\colorpalettes.h \
+ c:\\Users\\jgranat\\My\ Drive\\Dev\\Arduino\\libraries\\FastLED\\noise.h \
+ c:\\Users\\jgranat\\My\ Drive\\Dev\\Arduino\\libraries\\FastLED\\power_mgt.h \
+ c:\\Users\\jgranat\\My\ Drive\\Dev\\Arduino\\libraries\\FastLED\\fastspi.h \
+ c:\\Users\\jgranat\\My\ Drive\\Dev\\Arduino\\libraries\\FastLED\\fastspi_bitbang.h \
+ c:\\Users\\jgranat\\My\ Drive\\Dev\\Arduino\\libraries\\FastLED\\chipsets.h
